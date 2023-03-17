a5750ee04d779371f696476daea94c6434ba50e2
Juan Carlos Reyes <41979961+JuanCReyes1@users.noreply.github.com>
2023-03-17 15:27:04 -0300

    created acenet file
    acenet!!

---
+++ python38/laspy_acenet.py
@@ -0,0 +1,270 @@
+import laspy
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pyproj
+
+
+#used to load .las data into a laspy object.
+def load_laspy(las_location):
+      input_las = laspy.read(las_location)
+      
+      return input_las
+
+#used to return a list of unique classifications present in the input LAS file.
+def get_list_classifications(input_las):
+    classifications = list(set(input_las.classification))
+    classifications.sort()
+    
+    return classifications
+
+#Used to return a Python dictionary of laspy objects of the entire dataset as well as individual classifications.
+def get_classifications_laspy(input_las):
+    #get a unique list of the classifications currently contained in the dataset.
+    classifications = get_list_classifications(input_las=input_las)
+    #create dictionary with complete data for each province
+    d = {"original_dataset": input_las}
+    print("The classifications contained in this .las dataset are: ")
+    print(classifications)
+    print("Creating a python dictionary containing the entire dataset and individual classifications as separate laspy objects.")
+    for classification in classifications:
+        print("Saving classification number: " +str(classification))
+    
+        header = laspy.LasHeader(point_format=input_las.header.point_format, version=input_las.header.version)
+        header.scales = np.array([0.001, 0.001, 0.001])
+
+        classified_points = laspy.LasData(header)
+
+        epsg=2960
+        crs = pyproj.CRS.from_epsg(epsg)  # example: WGS84
+        classified_points.header.add_crs(crs)
+
+        #classified_points = laspy.create(point_format=input_las.header.point_format, file_version=input_las.header.version)
+
+        classified_points.points = input_las.points[input_las.classification == classification]
+
+        classified_points.header.offsets = input_las.header.offsets
+
+        
+        d["classification_{}".format(str(classification))] = classified_points
+    
+    return d
+
+#Optional function which takes the dictionary of laspy objects and converts every classification
+#   into individual .LAS files 
+def laspy_classifications_to_las(classifications_laspy):
+    for classification_las in classifications_laspy:
+        if classification_las == "original_dataset":
+            pass
+            print("skipped exporting the original dataset.")
+        else:
+            print("processing {} to a LAS file: ".format(classification_las))
+            print(classifications_laspy[classification_las])
+            classifications_laspy[classification_las].write("classification_{}.las".format(classification_las.split("_")[1]))
+            print("done")
+
+# Convert a .LAS file into a pandas object.
+def convert_laspy_pandas(input_las):
+    # Convert data into pandas DataFrame
+    df = pd.DataFrame({"X":input_las.X,"Y":input_las.Y,"Z":input_las.Z,
+      "x":np.array(input_las.x),"y":np.array(input_las.y),"z":np.array(input_las.z),
+     'intensity': input_las.intensity,
+      'classification': input_las.classification,
+      'return_number': np.array(input_las.return_number),
+      'number_of_returns':np.array(input_las.number_of_returns),
+      'synthetic':np.array(input_las.synthetic),
+      'key_point':np.array(input_las.key_point),
+      'withheld':np.array(input_las.withheld),
+      'overlap':np.array(input_las.overlap),
+      'scanner_channel':np.array(input_las.scanner_channel),
+      'scan_direction_flag':np.array(input_las.scan_direction_flag),
+      'user_data':input_las.user_data,
+      'scan_angle':input_las.scan_angle,
+      'point_source_id':input_las.point_source_id,
+      'gps_time':input_las.gps_time    
+      })
+
+    return df
+
+def get_selected_classes(classifications,input_las):
+    classes = get_list_classifications(input_las=input_las)
+    available_classes = classes
+    available_classes.append(1000) #to represent the entire dataset
+    class_selections = []
+    current_selection = -10
+
+    while current_selection == -10:
+        print("Available classifications: " + str(available_classes))
+        try:
+            current_selection = input("Enter a number from the list of available classifications. To get the full dataset enter 1000 : \n")
+            if current_selection.isnumeric() == False:
+                print("Invalid Character Input.")
+                raise ValueError()
+            current_selection = int(current_selection)
+            if current_selection not in available_classes:
+                print("The classification number you selected: {}, is not an available class item.".format(current_selection))
+                cancel = input("Would you like to exit instead? 'Y/N' ").upper() + "Y"
+                if cancel[0] == "Y":
+                    current_selection = -1000
+                    print("Exiting and returning empty classification selection list.")
+                    class_selections = []    
+                else: 
+                    raise ValueError()
+            if (current_selection != -1000) & (current_selection != 1000):
+
+                if 1000 in available_classes:
+                    available_classes.remove(1000)
+                class_selections.append(current_selection)
+                print("Class {} added to dataframe.".format(str(current_selection)))
+                available_classes.remove(current_selection)
+                ask = input("Would you like to add another classification to the dataframe? 'Y/N' ").upper() + "Y"
+
+                #If user prompts to add another classification, reset variable and start again.
+                if ask[0] == "Y":
+                    current_selection = -10
+                #If user prompts to stop, exit loop and create pandas dataframe.
+                else:
+                    print("Classifications have been selected.")
+                    class_selections.sort()
+
+            elif current_selection == 1000:
+                print("The entire dataset has been selected")
+                class_selections = get_list_classifications(input_las=input_las)
+                print("All classes have been added to the dataframe.")
+                class_selections.sort()
+
+        except ValueError:
+            print("Please select an input from the available classification list. \n")
+            current_selection = -10
+
+    
+    return class_selections
+
+def create_combined_classifications_dataframe(class_selections,df):
+    if len(class_selections) > 0:
+        #initialize the dataframe with the first set of classifications
+        combined_df = df.loc[df["classification"] == class_selections[0]]
+        #iterate and append remaining classifications 
+        for selection in class_selections[1:]:
+            combined_df=pd.concat([combined_df, df.loc[df["classification"] == selection]])
+    else:
+        combined_df = pd.DataFrame()
+
+    return combined_df
+
+
+def get_numpy_points_from_laspy_scaled(classified_points):
+    point_records = classified_points.points.copy()
+    # getting scaling and offset parameters
+    las_scaleX = classified_points.header.scale[0]
+    las_offsetX = classified_points.header.offset[0]
+    las_scaleY = classified_points.header.scale[1]
+    las_offsetY = classified_points.header.offset[1]
+    las_scaleZ = classified_points.header.scale[2]
+    las_offsetZ = classified_points.header.offset[2]
+    # calculating coordinates
+    p_X = np.array((point_records.X * las_scaleX) + las_offsetX) 
+    p_Y = np.array((point_records.Y * las_scaleY) + las_offsetY)
+    p_Z = np.array((point_records.Z * las_scaleZ) + las_offsetZ)
+
+    classified_points_numpy = np.array(list(zip(p_X,p_Y,p_Z)))
+
+    return classified_points_numpy
+
+
+def get_numpy_points_from_laspy_unscaled(classified_points):
+    point_records = classified_points.points.copy()
+    # calculating coordinates
+    p_X = np.array(point_records.X)
+    p_Y = np.array(point_records.Y)
+    p_Z = np.array(point_records.Z)
+
+    classified_points_numpy = np.array(list(zip(p_X,p_Y,p_Z)))
+
+    return classified_points_numpy
+
+def get_numpy_points_from_pandas_unscaled(combined_df):
+
+    classified_points_numpy = np.array(list(zip(np.array(combined_df["X"]),
+                                    np.array(combined_df["Y"]),
+                                    np.array(combined_df["Z"]))))
+
+    return classified_points_numpy
+
+def get_numpy_points_from_pandas_scaled(combined_df):
+
+    classified_points_numpy = np.array(list(zip(np.array(combined_df["x"]),
+                                    np.array(combined_df["y"]),
+                                    np.array(combined_df["z"]))))
+
+    return classified_points_numpy
+
+
+def create_laspy_from_dataframe(input_las,combined_df):
+    #classified_points = laspy.create(point_format=input_las.header.point_format, file_version=input_las.header.version)
+
+    #Create a header for the new .Las file
+    # Header class properties (https://laspy.readthedocs.io/en/latest/api/laspy.header.html)
+    header = laspy.LasHeader(point_format=input_las.header.point_format, version=input_las.header.version)
+    header.scales = np.array([0.001, 0.001, 0.001])
+
+    crs = pyproj.CRS.from_epsg(2960)  # example: WGS84
+    
+    classified_points = laspy.LasData(header)
+
+    classified_points.X = combined_df["X"]
+    classified_points.Y = combined_df["Y"]
+    classified_points.Z = combined_df["Z"]
+    classified_points.intensity = combined_df["intensity"]
+    classified_points.classification = combined_df["classification"]
+
+
+    #Apply the add_crs function to add a crs to our header.
+    classified_points.header.add_crs(crs)
+    #Select the offset from the input las data.
+    classified_points.header.offsets = input_las.header.offsets
+    #header.y_offset=0
+
+
+    return classified_points
+    
+
+def multiple_classifications_to_las(classified_points,class_selections):
+    ##write to a .las file
+    class_selections.sort()
+    sorted_classes_str = "_".join(map(str,class_selections))
+    las_file_name = "classifications_" + sorted_classes_str +".las"
+    classified_points.write(las_file_name)
+    print("Created file: " + sorted_classes_str)
+
+
+input_las = load_laspy('Tile65_Original_Clean_Macro.las')
+
+
+#input_las = load_laspy("200-145324069_ground.las")
+#Obtain the classifications available for the dataset
+classifications = get_list_classifications(input_las=input_las)
+#Create a python dictionary containing the original dataset
+# as well as classifications in individual laspy objects.
+classifications_laspy =  get_classifications_laspy(input_las=input_las)
+
+
+df = convert_laspy_pandas(input_las=input_las)
+
+
+#To get a pandas dataframe with specified classification values
+#Inform user which class values are present 
+#Proceed by prompting user input to select classes to group into a final dataframe.
+class_selections = get_selected_classes(classifications = classifications,input_las=input_las)
+# Form a pandas dataframe from user-selected classifications
+combined_df = create_combined_classifications_dataframe(class_selections=class_selections,df=df)
+
+##Create laspy points from the pandas dataframe of selected user classifications
+classified_points = create_laspy_from_dataframe(input_las=input_las,combined_df=combined_df)
+
+
+laspy_classifications_to_las(classifications_laspy=classifications_laspy)
+
+multiple_classifications_to_las(classified_points=classified_points,class_selections=class_selections)
+
+
